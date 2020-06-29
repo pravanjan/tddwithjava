9d810f7b4c9d38377a7d2df531f642dbcf3839d2
pravanjan <pravanjan.niranjan.anywhere.co>
2020-06-29 16:11:21 +0530

    added github action

--- src/test/java/reminder/Remindertest_backup.java
+++ src/test/java/reminder/Remindertest_backup.java
@@ -20,63 +20,60 @@ import org.mockito.runners.MockitoJUnitRunner;
 import reminderPojo.Reminder;
 
 @RunWith(MockitoJUnitRunner.class)
-public class Remindertest_backup{
-	
-@Mock
- private GooogleCalendarSync calsyncMock ;
+public class Remindertest_backup {
 
-@InjectMocks
-private  ReminderHelper helper ;
+	@Mock
+	private GooogleCalendarSync calsyncMock;
 
-@Before 
-public void setUp() throws Exception {
-	MockitoAnnotations.initMocks(this);
-}
+	@InjectMocks
+	private ReminderHelper helper;
 
-	
-  @Test
-  public void createReminderwithallvalidparam(){
-	  Reminder reminder = new Reminder("Breakfast", "Health", LocalDateTime.now().plusHours(1) );
-	   Map<String,Object> responseMap = new ReminderHelper().createReminder(reminder,calsyncMock);
-	   assertEquals(responseMap.get("message"),"created Successfully");
-	  }
-  
-  @Test
-  public void createReminderNamevalueasEmptyString(){
-	  
-	   Reminder reminder = new Reminder("", "Health", LocalDateTime.now().plusHours(1) );
-	   Map<String,Object> responseMap = new ReminderHelper().createReminder(reminder,calsyncMock);
-	   List<Reminder>  reminderList = (List<Reminder>) responseMap.get("reminderList");
-	   assertEquals("Default",reminderList.get(0).getName());
-	  }
-  
-  @Test
-  public void createReminderNamevalueasNull(){
-	  
-	   Reminder reminder = new Reminder(null, "Health", LocalDateTime.now().plusHours(1) );
-	   Map<String,Object> responseMap = new ReminderHelper().createReminder(reminder,calsyncMock);
-	   List<Reminder>  reminderList = (List<Reminder>) responseMap.get("reminderList");
-	   assertEquals("Default",reminderList.get(0).getName());
-	  }
-  
-  @Test
-  public void addEventToGoogleCalendar(){
-	  
-	   Reminder reminder = new Reminder("BreakFirst", "Health", LocalDateTime.now().plusHours(1) );
-	   when(calsyncMock.addEventtoGcal(any(Reminder.class))).thenReturn(true);	 
-	   
-	   Map<String,Object> responseMap = new ReminderHelper().createReminder(reminder,calsyncMock);
-	   List<Reminder>  reminderList = (List<Reminder>) responseMap.get("reminderList");
-	   assertEquals(true,reminderList.get(0).getGcalFlag());
-	  }
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+	}
 
-  @Test
-  public void createReminderInvalidDateparam(){
-	  Reminder reminder = new Reminder("Breakfast", "once", LocalDateTime.of(2018,Month.APRIL,20,12,30, 00) );
-	  Map<String,Object> responseMap  = new ReminderHelper().createReminder(reminder,calsyncMock);
-	  assertEquals(responseMap.get("message"),"reminder can not be set on past date");
-	 }
-  
-  
+	@Test
+	public void createReminderwithallvalidparam() {
+		Reminder reminder = new Reminder("Breakfast", "Health", LocalDateTime.now().plusHours(1));
+		Map<String, Object> responseMap = new ReminderHelper().createReminder(reminder, calsyncMock);
+		assertEquals(responseMap.get("message"), "created Successfully");
+	}
+
+	@Test
+	public void createReminderNamevalueasEmptyString() {
+
+		Reminder reminder = new Reminder("", "Health", LocalDateTime.now().plusHours(1));
+		Map<String, Object> responseMap = new ReminderHelper().createReminder(reminder, calsyncMock);
+		List<Reminder> reminderList = (List<Reminder>) responseMap.get("reminderList");
+		assertEquals("Default", reminderList.get(0).getName());
+	}
+
+	@Test
+	public void createReminderNamevalueasNull() {
+
+		Reminder reminder = new Reminder(null, "Health", LocalDateTime.now().plusHours(1));
+		Map<String, Object> responseMap = new ReminderHelper().createReminder(reminder, calsyncMock);
+		List<Reminder> reminderList = (List<Reminder>) responseMap.get("reminderList");
+		assertEquals("Default", reminderList.get(0).getName());
+	}
+
+	@Test
+	public void addEventToGoogleCalendar() {
+
+		Reminder reminder = new Reminder("BreakFirst", "Health", LocalDateTime.now().plusHours(1));
+		when(calsyncMock.addEventtoGcal(any(Reminder.class))).thenReturn(true);
+
+		Map<String, Object> responseMap = new ReminderHelper().createReminder(reminder, calsyncMock);
+		List<Reminder> reminderList = (List<Reminder>) responseMap.get("reminderList");
+		assertEquals(true, reminderList.get(0).getGcalFlag());
+	}
+
+	@Test
+	public void createReminderInvalidDateparam() {
+		Reminder reminder = new Reminder("Breakfast", "once", LocalDateTime.of(2018, Month.APRIL, 20, 12, 30, 00));
+		Map<String, Object> responseMap = new ReminderHelper().createReminder(reminder, calsyncMock);
+		assertEquals(responseMap.get("message"), "reminder can not be set on past date");
+	}
 
 }
